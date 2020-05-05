0cef2e4e352c8cf551fc7a010c28ed8ba72b32e9
Lucas Fanovich <lucas@zibracoders.co>
2020-05-04 22:12:25 -0300

    initial commit

---
+++ Albums/Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by Zibra Coders on 04/05/2020.
+//  Copyright © 2020 Fanovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
