5fd1deab9744086740921a7e241442c354a515c9
Lucas Fanovich <fanovichlucas@gmail.com>
2020-05-05 19:23:21 -0300

    adding search bar to albums view controller

--- Albums/Albums/Modules/AlbumsModule/View/AlbumsViewController.swift
+++ Albums/Albums/Modules/AlbumsModule/View/AlbumsViewController.swift
@@ -13,13 +13,16 @@ import UIKit
 class AlbumsViewController: UIViewController, AlbumsViewProtocol {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
 	var presenter: AlbumsPresenterProtocol?
+    var filteredAlbumsArray = [Album]()
 
 	override func viewDidLoad() {
         super.viewDidLoad()
         title = "Album list"
         configureTableView()
+        configureSearchBar()
     }
     override func viewDidAppear(_ animated: Bool) {
         updateTableView()
@@ -31,12 +34,17 @@ class AlbumsViewController: UIViewController, AlbumsViewProtocol {
         tableView.register(UINib(nibName: "AlbumTableViewCell", bundle: nil), forCellReuseIdentifier: "AlbumCell")
     }
     
+    func configureSearchBar() {
+        searchBar.delegate = self
+    }
+    
     func showAlbums() {
+        filteredAlbumsArray = presenter?.list ?? [Album]()
         tableView.reloadData()
     }
     
     func showError(_ error: String) {
-        
+        print(error)
     }
     
     func updateTableView() {
@@ -52,20 +60,31 @@ extension AlbumsViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return presenter?.list?.count ?? 0
+        return filteredAlbumsArray.count//presenter?.list?.count ?? 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "AlbumCell") as! AlbumTableViewCell
-        if let list = presenter?.list {
-            cell.configure(with: list[indexPath.section])
-        }
+        cell.configure(with: filteredAlbumsArray[indexPath.section])
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let album = presenter?.list?[indexPath.section] {
-            self.presenter?.showPhotosViewController(with: album.id)
+        self.presenter?.showPhotosViewController(with: filteredAlbumsArray[indexPath.section].id)
+    }
+}
+
+extension AlbumsViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard let albums = presenter?.list else  { return }
+        if searchText.isEmpty {
+            filteredAlbumsArray = albums
+            return
         }
+        filteredAlbumsArray = albums.filter({ (album) -> Bool in
+            album.title.contains(searchText.lowercased())
+        })
+        tableView.reloadData()
     }
 }
